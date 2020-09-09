e218d297d53ccf400b9e1b7aefe6acdb64df9319
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-09 11:20:50 -0700

    push repo

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,4 @@ const router = require('express').Router()
 
 router.use('/api', require('./itemRoutes.js'))
 
-module.exports = 
+module.exports = router
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,26 +1,32 @@
 const router = require('express').Router()
-const { item } = require('../models')
+const { Item } = require('../models')
 
 // GET all items 
 router.get('/items', (req, res) => {
-    item.find()
-        .then(items => )
+    Item.find()
+        .then(items => res.json(items))
         .catch(err => console.log(err))
 })
 
 // POST one item 
 router.post('/items', (req, res) => {
-
+    Item.create(req.body)
+        .then(item => res.json(item))   
+        .catch(err => console.log(err)) 
 })
 
 // PUT one item
 router.put('./items/:id', (req, res) => {
-
+    Item.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
 })
 
 // DELETE one item 
 router.delete('./items/:id', (req, res) => {
-
+    Item.findByIdAndDelete(req.params.id, req.body)
+    .then(() => res.sendStatus(200))
+    .catch(err => console.log(err))
 })
 
 module.exports = router
