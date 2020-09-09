c56af111bd588aeb60e47de75c896ebc5c4dff86
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-09 10:51:25 -0700

    git

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { join } = require('path')
 const app = express()
 
 app.use(express.static(join(__dirname, 'public')))
-app.use(express.urlencoded({ extended: true }))
+app.use(expresnodes.urlencoded({ extended: true }))
 app.use(express.json())
 
 require('./db')
