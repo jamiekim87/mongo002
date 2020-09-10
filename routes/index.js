128b161b8ba5fc03423b10fb25d180e75c9ea87d
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-09-09 21:03:29 -0700

    code

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router()
 
 router.use('/api', require('./itemRoutes.js'))
-
-const router = require('express').Router()
-
-router.use('/api', require('./itemRoutes.js'))
 router.use('/api', require('./userRoutes.js'))
 
 module.exports = router
